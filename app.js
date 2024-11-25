9aab7b1a1e0b03f6dfd9f984d495b3d3b04b28d5
Ibrahim Shariff <imys@Ibrahims-MBP.northcoders.com>
2024-11-25 15:21:19 +0000

    complete till 3

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
-const getApi = require("./controllers/api.controllers");
+const { getApi, getAllTopics } = require("./controllers/api.controllers");
 
 app.get("/api", getApi);
-
+app.get("/api/topics", getAllTopics);
+app.all("*", (req, res) => {
+  res.status(404).send({ msg: "Not Found" });
+});
 module.exports = app;
--- controllers/api.controllers.js
+++ controllers/api.controllers.js
@@ -1,8 +1,20 @@
 const endpointsJSON = require("../endpoints.json");
+const { fetchTopics } = require("../models/models");
 
 function getApi(req, res) {
   console.log(endpointsJSON, "<----- enpoints.json");
   res.status(200).send({ endpoints: endpointsJSON });
 }
 
-module.exports = getApi;
+function getAllTopics(req, res, next) {
+  fetchTopics()
+    .then((topics) => {
+      console.log(topics);
+      res.status(200).send({ topics });
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+module.exports = { getApi, getAllTopics };
